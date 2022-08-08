9f6f9659631c3d7b60fb81ecd285302ef4fc038b
cuongvv205 <101417205+cuongvv205@users.noreply.github.com>
2022-08-08 20:29:18 +0700

    update

--- routes/index.js
+++ routes/index.js
@@ -1,78 +1,98 @@
 var express = require('express');
-const app = express();
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const bodyParser = require("body-parser");
-var fs  = require('fs');
-var multer =require('multer');
-
-app.use(bodyParser.urlencoded(
-    { extended:true }
-))
-app.set("view engine","ejs");
-var storage = multer.diskStorage({
-  destination: function (req, file, cb){
-    cb(null, 'uploads/');
-  },
-  filename: function (req, file, cb) {
-    cb(null, Date.now() + file.originalname);
-  },
-});
-var upload = multer({storage: storage});
-
 var router = express.Router();
-const mongoose = require("mongoose");
+var mongoose = require('mongoose');
 const {Schema} = require("mongoose");
 const e = require("express");
-
+//  bước 1 : kết nối vào csdl qua link với username, password
+// tên của csdl là demo
 const uri = "mongodb+srv://admin:cuong2001@cluster0.jsesy.mongodb.net/poly1?retryWrites=true&w=majority";
-mongoose.connect(uri).catch(err => console.log('co loi xay ra: ' + err));
+mongoose.connect(uri).catch(err => console.log('co loi xay ra'));
 
-var IMAGE = mongoose.Schema({
-  content: String,
-  date: String,
-  img: {
-    data: Buffer,
-    contentType: String
-  }
+// bước 2 : khai báo kiểu dữ liệu - khung của dữ liệu - model và collection
+// _id:62e229de28a3a68729705ef0
+// name:"Huy Nguyen Huun3333"
+// number:"0913456789"
+// address:"HaNoi city"
+
+const itemSchema = new Schema({
+  noidung: String,
+  ngay: String,
+  link: String
 })
-var IMG =mongoose.model('images', IMAGE);
 
+const itemModel = mongoose.model('images', itemSchema)
+
+/* kết nối và lấy danh sách */
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  IMG.find({}, function (error, result) {
+router.get('/', function (req, res, next) {
+
+  // tim kiem : dk {}
+  itemModel.find({}, function (error, result) {
     if (error) throw error;
-    console.log(error)
     console.log(result.length)
-    res.render('index', {title: 'Express', items: result});
+    res.render('index', {title: 'Express', data: result});
   })
 });
+router.get('/delete/', function (req, res) {
 
-router.get('/getImage', function (req, res) {
-  const imgList = mongoose.model('images', IMAGE);
-
-  imgList.find({}, function (error, result) {
-    res.send(result);
+  const id = req.query.id;
+  itemModel.deleteOne({_id: id}, function (error) {
+    if (error) throw error;
+    res.send('Xoa thanh cong!!!');
   })
+
 })
 
-router.post('/uploadphoto', upload.single('avatar'), function (req, res) {
-  var img = fs.readFileSync(req.file.path);
-  var encode_img = img.toString('base64');
-  var final_img = {
-    contentType:req.file.mimetype,
-    image:new Buffer(encode_img,'base64')
-  };
-  console.log("post")
-  IMG.create(final_img,function(err,result){
-    if(err){
-      console.log(err);
-    }else{
-      console.log(result.img.Buffer);
-      console.log("Saved To database");
-      res.contentType(final_img.contentType);
-      res.send(final_img.image);
-      res.redirect("/");
+router.get('/updateForm/', function (req, res) {
+
+  const id = req.query.id;
+  itemModel.findOne({_id: id}, function (error, result) {
+    res.render('update', {title: 'Update', data: result})
+  })
+
+})
+
+router.post('/update', async function (req, res) {
+  const id = req.body.id;
+  const noidung = req.body.noidung;
+  const ngay = req.body.ngay;
+  const link = req.body.link;
+
+
+  await itemModel.updateOne({_id: id}, {
+    noidung: noidung,
+    ngay: ngay,
+    link: link
+  }, null)
+
+  res.redirect('/')
+})
+
+router.post('/create', async function (req, res) {
+  const id = req.body.id;
+  const noidung = req.body.noidung;
+  const ngay = req.body.ngay;
+  const link = req.body.link;
+
+  var item = new itemModel({
+    noidung: noidung,
+    ngay: ngay,
+    link: link
+  })
+
+  await item.save();
+
+  res.redirect('/')
+})
+
+router.get('/getUsers', function (req, res) {
+  const itemList = mongoose.model('images', itemSchema);
+
+  itemList.find({}, function (error, result) {
+    if(error){
+      console.log(error)
     }
+    res.send(result);
   })
 })
 
